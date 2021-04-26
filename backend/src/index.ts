f4ea5c9a3d27caec5160d4720b54da3fb76a643a
Lucas Borges <45474687+lucasborgesdev@users.noreply.github.com>
2021-04-26 10:14:23 -0300

    login facebook and google ok

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,11 @@ import cors from 'cors';
 import session from 'express-session';
 import passport from 'passport';
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const TwitterStrategy = require('passport-twitter').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const GitHubStrategy = require('passport-github').Strategy;
+const InstagramStrategy = require('passport-instagram').Strategy;
+
 
 
 
@@ -39,7 +44,12 @@ passport.serializeUser((user:any, done:any) => {
 
 passport.deserializeUser((user:any, done:any) =>{
   return done(null, user);
-})
+});
+
+// First Name
+// Last Name
+// Id of Oauth system
+
 
 
 passport.use(new GoogleStrategy({
@@ -47,30 +57,128 @@ passport.use(new GoogleStrategy({
   clientSecret: `${process.env.CLIENTE_SECRET_GOOGLE}`,
   callbackURL: "/auth/google/callback"
 },
+
 function(accessToken: any, refreshToken: any, profile: any, cb: any) {
   // Chamada de requisição efetuada com sucesso!
   //Inserir do db
-
   console.log(profile);
   cb(null,profile);
 }));
 
-app.get('/auth/google',
+passport.use(new TwitterStrategy({
+  consumerKey: "TWITTER_CONSUMER_KEY",
+  consumerSecret: "TWITTER_CONSUMER_SECRET",
+  callbackURL: "/auth/twitter/callback"
+},
+function(accessToken: any, refreshToken: any, profile: any, cb: any) {
+  // Chamada de requisição efetuada com sucesso!
+  //Inserir do db
+  console.log(profile);
+  cb(null,profile);
+}));
+
+passport.use(new FacebookStrategy({
+  //clientID: "466559504470748",
+  //clientSecret: "49faad6f279ff23f7e3f29b1afeb7fc1",
+  clientID: `${process.env.ID_CLIENTE_FACEBOOK}`,
+  clientSecret: `${process.env.CLIENTE_SECRET_FACEBOOK}`,
+  callbackURL: "/auth/facebook/callback"
+},
+function(accessToken: any, refreshToken: any, profile: any, cb: any) {
+  // Chamada de requisição efetuada com sucesso!
+  //Inserir do db
+  console.log(profile);
+  cb(null,profile);
+}));
+
+passport.use(new GitHubStrategy({
+  clientID: "fe7ead31c31b0152900c",
+  clientSecret: "1f55f85a2519173c4fd7ac0c4cb95b67f3097433",
+  callbackURL: "http://localhost:4000/auth/github/callback"
+},
+function(accessToken: any, refreshToken: any, profile: any, cb: any) {
+  // Chamada de requisição efetuada com sucesso!
+  //Inserir do db
+  console.log(profile);
+  cb(null,profile);
+}));
+
+
+
+
+passport.use(new InstagramStrategy({
+  clientID: `${process.env.INSTAGRAM_CLIENT_ID}`,
+  clientSecret: `${process.env.INSTAGRAM_CLIENT_SECRET}`,
+  callbackURL: "http://localhost:4000/auth/instagram/callback"
+},
+function(accessToken: any, refreshToken: any, profile: any, cb: any) {
+  // Chamada de requisição efetuada com sucesso!
+  //Inserir do db
+  console.log(profile);
+  cb(null,profile);
+}));
+
+
+
+  app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile'] }));
 
-app.get('/auth/google/callback', 
+  app.get('/auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:3000');
+  });
+
+  app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+  app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:3000');
+  });
+
+  app.get('/auth/twitter', passport.authenticate('twitter'));
+
+  app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:3000');
+  });
+
+  app.get('/auth/github',
+  passport.authenticate('github'));
+
+  app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
     res.redirect('/');
   });
 
+  app.get('/auth/instagram',
+  passport.authenticate('instagram'));
 
-app.get("/", (req,res) =>{
-  res.send("Olá mundo !!!")
-}
-)
+  app.get('/auth/instagram/callback', 
+  passport.authenticate('instagram', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:3000');
+  });
 
-app.listen(4000, () => {
-  console.log("Pai tá ON!")
-})
+  app.get("/", (req,res) =>{
+    res.send("Olá mundo !!!")
+  });
+
+  app.get("/getuser", (req,res) => {
+    
+    res.send(req.user);
+  })
+
+
+  app.listen(4000, () => {
+    console.log("Pai tá ON!")
+});
--- client/src/Components/LoginPage/LoginPage.tsx
+++ client/src/Components/LoginPage/LoginPage.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import Google from '../../assets/Logo/Google.png';
+import googleImage from '../../assets/Logo/Google.png';
+import githubImage from '../../assets/Logo/github.png';
+import twitterImage from '../../assets/Logo/twitterImage.png';
+import facebookImage from '../../assets/Logo/facebook-512.png';
+import instagramImage from '../../assets/Logo/insta2.png';
 import styles from './LoginPage.module.css';
 
 
 const googleLogin = () => {
   window.open("http://localhost:4000/auth/google", "_self");
 }
+
+const githublogin = () => {
+  window.open("http://localhost:4000/auth/github", "_self");
+}
+
+const twitterlogin = () => {
+  //window.open("http://localhost:4000/auth/githublogin", "_self");
+}
+const facebooklogin = () => {
+  window.open("http://localhost:4000/auth/facebook", "_self");
+}
+
+const instagramlogin = () => {
+  window.open("http://localhost:4000/auth/instagram", "_self");
+}
+
 
 
 export default function LoginPage() {
@@ -17,8 +37,24 @@ export default function LoginPage() {
           <h1>Login</h1>
       </div>
       <div className={styles.googleContainer} onClick={googleLogin}>
-        <img src={Google} alt="Google Icon" />
+        <img src={googleImage} alt="Google Icon" />
         <p>Login with Google</p>
+      </div>
+      <div className={`${styles.googleContainer} ${styles.githubContainer}`} onClick={githublogin}>
+        <img src={githubImage} alt="GitHub Icon" />
+        <p>Login with GitHub</p>
+      </div>
+      <div className={`${styles.googleContainer} ${styles.twitterContainer}`} onClick={twitterlogin}>
+        <img src={twitterImage} alt="Twitter Icon" />
+        <p>Login with Twitter</p>
+      </div>
+      <div className={`${styles.googleContainer} ${styles.facebookContainer}`} onClick={facebooklogin}>
+        <img src={facebookImage} alt="Twitter Icon" />
+        <p className={styles.facebookText}>Login with Facebook</p>
+      </div>
+      <div className={`${styles.googleContainer} ${styles.InstagramContainer}`} onClick={instagramlogin}>
+        <img src={instagramImage} alt="Twitter Icon" />
+        <p className={styles.facebookText}>Login with Instagram</p>
       </div>
     </div>
   )
